00599bdcceb4f2520abc5116f2e72d4744586b11
Marcos Paulo <marcosgona13@gmail.com>
2022-09-03 21:13:32 -0300

    Marcos

--- pages/index.tsx
+++ pages/index.tsx
@@ -3,20 +3,21 @@ import io from 'socket.io-client';
 
 import type { NextPage } from "next";
 let socket;
+
 const Home: NextPage = () => {
-  useEffect(() => {
-    socketInitializer();
-  }, []);
+   useEffect(() => {
+      socketInitializer();
+   }, []);
 
-  const socketInitializer = async () => {
-    await fetch("/api/socket");
-    socket = io();
-    socket.on("newIncomingMessage", (msg) => {
-      console.log(msg);
-    });
-  };
+   const socketInitializer = async () => {
+      await fetch("/api/socket");
+      socket = io();
+      socket.on("newIncomingMessage", (msg) => {
+         console.log(msg);
+      });
+   };
 
-  return <h1>Heloo</h1>;
+   return <h1>Heloo</h1>;
 };
 
 export default Home;
